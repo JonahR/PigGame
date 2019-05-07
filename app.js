76bd6d695d6a16845743e22da48d928861b5821d
Jonah Rueb <ruebjonah@gmail.com>
2019-05-07 13:13:19 -0500

    DRY

--- app.js
+++ app.js
@@ -42,22 +42,41 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         // Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-0').textContent = '0';
-        
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        
-        //document.querySelector('.player-0-panel').classList.remove('active');
-        //document.querySelector('.player-1-panel').classList.add('active');
-        
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 });
 
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // Add current score to global score
+    scores[activePlayer] += roundScore;
+    
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    // Check if player won the game
+    
+    // Next player
+    nextPlayer();   
+});
+
+function nextPlayer() {
+    
+    // Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+        
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+        
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+        
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('.player-1-panel').classList.add('active');
+        
+    document.querySelector('.dice').style.display = 'none';    
+}
 
 
 //dice = Math.floor(Math.random() * 6) + 1;
